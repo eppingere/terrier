c3897db291296a12a4baec0bd969c658c7062494
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-10-06 19:25:24 -0400

    update benchmark

--- benchmark/common/tbb_benchmark.cpp
+++ benchmark/common/tbb_benchmark.cpp
@@ -1,10 +1,12 @@
 #include <gbenchmark_ep/src/gbenchmark_ep-install/include/benchmark/benchmark.h>
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
 #include <tbb/task_arena.h>
 
 #include <exception>
 #include <iostream>
+#include <random>
 #include <thread>
 
 #include "common/scoped_timer.h"
@@ -123,6 +125,53 @@ BENCHMARK_DEFINE_F(TBBBENCHMARK, WorkerPoolBasic)(benchmark::State &state){
   delete [] array;
 
 }
-BENCHMARK_REGISTER_F(TBBBENCHMARK, TBBBasic)->Iterations(10)->Unit(benchmark::kMillisecond);
-BENCHMARK_REGISTER_F(TBBBENCHMARK, WorkerPoolBasic)->Iterations(10)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_DEFINE_F(TBBBENCHMARK, TBBBasicNoAllocation)(benchmark::State &state) {
+  // 500 MB array.
+  std::vector<uint8_t> array(500 * 1024 * 1024);
+
+  // Fill with garbage.
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_int_distribution<uint8_t> dist;
+  std::generate(array.begin(), array.end(), [&]() { return dist(gen); });
+
+  // Num threads from arguments.
+  const uint32_t num_threads = state.range(0);
+
+  for (auto _ : state) {
+    // Create thread pool.
+    tbb::task_arena arena(num_threads);
+
+    // Launch.
+    uint64_t sum = arena.execute([&] {
+      return tbb::parallel_reduce(
+          tbb::blocked_range<std::vector<uint8_t>::const_iterator>(array.begin(), array.end()),
+          // Identity element
+          uint64_t(0),
+          // Reduce a subrange and partial sum
+          [&](const auto &range, uint64_t partial_sum) -> float {
+            return std::accumulate(range.begin(), range.end(), partial_sum);
+          },
+          // Reduce two partial sums
+          std::plus<uint64_t>());
+    });
+    benchmark::DoNotOptimize(sum);
+  }
+}
+
+namespace {
+
+  static void CustomArguments(benchmark::internal::Benchmark *b) {
+    for (int64_t num_threads = 1; num_threads <= std::thread::hardware_concurrency(); num_threads++) {
+      b->Args({num_threads});
+    }
+  }
+
+}  // namespace
+
+
+//BENCHMARK_REGISTER_F(TBBBENCHMARK, TBBBasic)->Iterations(5)->Unit(benchmark::kMillisecond);
+//BENCHMARK_REGISTER_F(TBBBENCHMARK, WorkerPoolBasic)->Iterations(5)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(TBBBENCHMARK, TBBBasicNoAllocation)->Apply(CustomArguments)->Iterations(50)->Unit(benchmark::kMillisecond);
+
 }
